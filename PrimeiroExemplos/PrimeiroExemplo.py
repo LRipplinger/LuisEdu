d5b8a816cf8cbfcc84046bf9eeb336412a236b2b
LRipplinger <162515197+LRipplinger@users.noreply.github.com>
2024-03-06 21:51:13 -0300

    Add files via upload

---
+++ PrimeiroExemplos/PrimeiroExemplo.py
@@ -0,0 +1,22 @@
+# Comando De Saida
+print("Hello!")
+
+# Criando uma variável numérica e atribuindo um valor
+num = 10
+
+# Imprimindo o valor da variável num
+print (num)
+
+# Trocando o valor da variável num
+num = -1
+
+print (num)
+
+# leitura de comando (de entrada)
+nome = input('Digite seu nome e pressione ENTER: ')
+print(nome)
+
+# leitura de valor inteiro para variável idade
+idade = int(input('digite sua idade: '))
+
+print('Seu nome é ',nome,'e sua idade é ',idade)
